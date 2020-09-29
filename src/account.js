1dffdb51d770ce75dd187a52e672abf903382eac
unknown <baitik-jump@yandex.ru>
2020-09-29 13:44:10 +0300

    fix

--- src/account.js
+++ src/account.js
@@ -4,17 +4,17 @@ export const Account = (props) => {
     <div className= 'account'>
      <div className= 'account-information' onClick={() => props.onclick(6)}>
      <div className= 'account-letters'>
-     <div  ClassName= 'nickname-and-address'>
+
 
      <h5 className= 'nickname-letter'>Nickname</h5>
-
+<br/><br/>
       <h5 className= 'address-letter'>Адрес</h5>
-      </div>
-      <div ClassName= 'role-and-contacts'>
-      <h5 className= 'role-letter'>Роль</h5>
+<br/><br/>
 
+      <h5 className= 'role-letter'>Роль</h5>
+<br/><br/>
       <h5 className= 'contacts-letter'>Контакты</h5>
-      </div>
+    <br/><br/>
 
       <h5 className= 'information-letter'>Информация</h5>
        </div>
--- src/meetings.js
+++ src/meetings.js
@@ -2,7 +2,13 @@ import React from 'react';
 export const Meetings = () => {
   return (
     <div className='meetings'>
-    meetings
+    <h3>Встречи</h3>
+    <div className= 'meetings-filter'>
+    <h5 className= 'fiter-letter'></h5>
+    </div>
+    <div className= 'meetings-recomendation'>
+
+    </div>
     </div>
   )
 }
